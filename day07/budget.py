ff483f795b62bee74e6050e9fd4cfe49b735ff9b
AiRoboCop <levis_kim@nate.com>
2026-06-24 16:57:58 +0900

    commit in budget 가계부 class

--- day07/budget.py
+++ day07/budget.py
@@ -13,6 +13,39 @@ class Account:
 
     def deposit(self, amount):
         """입금 메서드"""
+        self.balance += amount
+        self.history.append(f"입금: +{amount}원")
+        print(f"{amount}원 입금 완료. 잔액: {self.balance}원")
+
+    def withdraw(self, amount):
+        """출금 메서드(잔액 부족 체크!)"""
+        if amount > self.balance:
+            print(f"잔액 부족 (현재 잔액: {self.balance}원)")
+            return
+
+        self.balance -= amount
+        self.history.append(f"출금: -{amount}원")
+        print(f"{amount}원 출금 완료. 잔액: {self.balance}원")
+
+    def show_balance(self):
+        """잔액 조회 메서드"""
+        print(f"{self.owner}님의 잔액은 {self.balance}원 입니다.")
+
+    def show_history(self):
+        """거래 내역"""
+        print(f"{self.owner}님의 거래 내역:")
+        if not self.history:
+            print("(거래 없음)")
+        for record in self.history:
+            print(f"- {record}")
+
+# === 테스트 ===
+acc = Account("김윤종")
+acc.deposit(10000)
+acc.withdraw(3000)
+acc.withdraw(50000) #잔액 부족 테스트
+acc.show_balance()
+acc.show_history()
 
 
 
