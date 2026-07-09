edf80f2602a9fb628ce43caa478919f9c4355810
AiRoboCop <levis_kim@nate.com>
2026-07-09 13:36:07 +0900

    feat : first UI coding

--- day09/first_window.py
+++ day09/first_window.py
@@ -4,16 +4,16 @@ import tkinter as tk
 root = tk.Tk()
 
 # 창 제목 정하기
-root.title("내 첫 GUI 프록램")
+root.title("내 첫 GUI 프로그램")
 
 # 창 크기 정하기(가로 * 세로, 픽셀)
 root.geometry("400x300")
-
-# 창 유지하기(이게 없으면 바로 사라짐)
-root.mainloop()
 
 label = tk.Label(root, text="안녕하세요!")
 label.pack()
 
 button = tk.Button(root, text="클릭하세요")
 button.pack()
+
+# 창 유지하기(이게 없으면 바로 사라짐)
+root.mainloop()
